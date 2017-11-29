1e319a18d216a5cc28bbd63fc5ae788c49646a9b
Ecksi <jimmy.dechant@gmail.com>
2017-11-29 14:21:00 -0700

    add schema

---
+++ db/migrate/20171129211509_create_station.rb
@@ -0,0 +1,10 @@
+class CreateStation < ActiveRecord::Migration[5.1]
+  def change
+    create_table :stations do |t|
+      t.string  :name
+      t.integer :dock_count
+      t.string  :city
+      t.date    :installation_date
+    end
+  end
+end
