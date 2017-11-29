23c27e192c885de10700292ae2dbaa3975ce300e
Ecksi <jimmy.dechant@gmail.com>
2017-11-29 15:00:08 -0700

    it_1 controller methods

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -5,7 +5,37 @@ class BikeShareApp < Sinatra::Base
     erb :"stations/index"
   end
 
+  get '/stations/:id' do
+    @station = Station.find(params[:id])
+
+    erb :"stations/show"
+  end
+
+  get '/stations/:id/edit' do
+    erb :"station/edit"
+  end
+
   get '/stations/new' do
-    
+    erb :"stations/new"
+  end
+
+  post '/stations' do
+    station = Station.create(params[:station])
+
+    redirect :"station/#{station.id}"
+  end
+
+  put '/stations' do
+    station = Station.update(params[:id], params[:station])
+
+    redirect :"station/#{station.id}"
+  end
+
+  delete '/stations/:id' do
+    # station = Station.find(params[:id])
+    # station.destroy
+    Station.destroy(params[:id])
+
+    redirect :"stations/index"
   end
 end
