6c291953013d135470e36e0323b7e1ba2c4699f4
maxscores <maxstackhouse@gmail.com>
2017-11-30 13:09:19 -0700

    Add show page test

---
+++ spec/features/user_visits_stations_id_spec.rb
@@ -0,0 +1,34 @@
+describe "When a visitor visits station show" do
+  before(:each) do
+    @station_1 = Station.create(name: "South SF", city: "SF", dock_count: 12, installation_date: "2017-01-01")
+    @station_2 = Station.create(name: "North SJ", city: "SJ", dock_count: 14, installation_date: "2017-01-10")
+  end
+
+  it "sees the correct station" do
+    visit '/stations/1'
+
+    expect(page).to have_current_path('/stations/1')
+
+    expect(page).to have_content("South SF")
+    expect(page).to have_content("SF")
+    expect(page).to have_content("12")
+    expect(page).to have_content("2017-01-01")
+  end
+
+  it "sees an Edit link" do
+    visit '/stations/1'
+
+    expect(page).to have_current_path('/stations/1')
+
+    expect(page).to have_link('Edit')
+  end
+
+  it "sees banner" do
+    visit '/stations'
+
+    expect(page).to have_current_path('/stations')
+
+    expect(page).to have_content("SF Bike Share")
+    expect(page).to have_link("Station Dashboard")
+  end
+end
