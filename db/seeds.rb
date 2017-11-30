46e0c96f633798066d2db9c672df31197f8fc940
seoulstice <jungyoungjoon@gmail.com>
2017-11-29 19:10:15 -0700

    Successfully seeded Station table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,9 @@
 require 'csv'
 require './app/models/station'
+
+CSV.foreach('db/csv/station.csv', {headers: true, header_converters: :symbol}) do |row|
+  Station.create(name:              row[:name],
+                 city:              row[:city],
+                 dock_count:        row[:dock_count],
+                 installation_date: row[:installation_date])
+end
