117efb45fe908ef8d5ef09be0942d61b3789a821
maxscores <maxstackhouse@gmail.com>
2017-12-01 18:30:09 -0700

    station instance methods setup

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -1,31 +1,75 @@
 RSpec.describe Station do
   describe "Instance Methods" do
+    before(:each) do
+      skip
+      Trip.create(duration: 2,
+                  start_date: "1-19-2017",
+                  start_station: "South SF",
+                  start_station_id: 1,
+                  end_date: "1-19-2017",
+                  end_station: "North SF",
+                  end_station_id: 2,
+                  bike_id: 21,
+                  subscription_type: "Subscriber",
+                  zipcode: 12345
+                  )
+
+      Trip.create(duration: 6,
+                  start_date: "7-4-2017",
+                  start_station: "North SF",
+                  start_station_id: 2,
+                  end_date: "7-4-2017",
+                  end_station: "North SF",
+                  end_station_id: 2,
+                  bike_id: 21,
+                  subscription_type: "Subscriber",
+                  zipcode: 12345
+                  )
+
+      Trip.create(duration: 4,
+                  start_date: "7-4-2017",
+                  start_station: "North SF",
+                  start_station_id: 2,
+                  end_date: "7-4-2017",
+                  end_station: "North SF",
+                  end_station_id: 2,
+                  bike_id: 33,
+                  subscription_type: "Customer",
+                  zipcode: 12345
+                  )
+
+      Station.create(name: "South SF", dock_count: 55, city: "San Juniperno", installation_date: "11/21/1992")
+      Station.create(name: "North SF", dock_count: 55, city: "San Juniperno", installation_date: "11/21/1992")
+      station_1 = Station.find(1)
+      station_2 = Station.find(2)
+    end
+
     it '#rides_started' do
-      expect(station.rides_started).to eq(999)
+      expect(station_1.rides_started).to eq(1)
     end
 
     it '#rides_ended' do
-      expect(station.rides_ended).to eq(999)
+      expect(station_2.rides_ended).to eq(3)
     end
 
     it '#most_popular_end_station' do
-      expect(station.most_popular_end_station.id).to eq(1)
+      expect(station_2.most_popular_end_station.id).to eq(2)
     end
 
     it '#most_popular_start_station' do
       expect(station.most_popular_start_station.id).to eq(2)
     end
 
-    it '#most_rides_started' do
-      expect(station.most_rides_started).to eq('7-4-2017')
+    it '#date_most_rides_started' do
+      expect(station_2.date_most_rides_started).to eq('7-4-2017')
     end
 
     it '#most_common_zipcode' do
-      expect(station.most_common_zipcode).to eq(12345)
+      expect(station_2.most_common_zipcode).to eq(12345)
     end
 
     it '#bike_most_commonly_started_on' do
-      expect(station.bike_most_commonly_started_on).to eq(21)
+      expect(station_1.bike_most_commonly_started_on).to eq(21)
     end
   end
 
