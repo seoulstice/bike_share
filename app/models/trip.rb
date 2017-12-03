57ae4b8273d9eb543b1731fd4b3ab365b9151a09
seoulstice <jungyoungjoon@gmail.com>
2017-12-02 19:55:04 -0700

    Most popular trip date and count of trips on that date method written in model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -9,6 +9,15 @@ class Trip <ActiveRecord::Base
                         :bike_id,
                         :subscription_type
 
+  def self.most_popular_day
+    date = Trip.group(:start_date).order('count(*) DESC').pluck(:start_date).first
+    Trip.most_popular_trip_date_count(date)
+  end
+
+  def self.most_popular_trip_date_count(date)
+    count = Trip.group(:start_date).order('count(*) DESC').count[date]
+    "#{date} with #{count} trips."
+  end
 
   def start_station_latitude
     Station.find(start_station_id).latitude
@@ -29,6 +38,9 @@ class Trip <ActiveRecord::Base
   scope :longest_ride, -> {maximum(:duration)}
   scope :shortest_ride, -> {minimum(:duration)}
   scope :average_ride_length, -> {average(:duration).to_f.round(2)}
-
+  scope :most_popular_starting_station, -> {maximum(:start_station)}
+  scope :most_popular_ending_station, -> {maximum(:end_station)}
+  # scope :most_popular_trip_date, -> {group(:start_date).order('count(*) DESC').limit(1).pluck(:start_date)}
+  # scope :most_popular_trip_date_count, -> {group(:start_date).order('count(*) DESC').limit(1) }
 
 end
