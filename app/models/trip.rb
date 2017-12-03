4db7846c273562eacf09ae42bc0cf445bcd758e3
seoulstice <jungyoungjoon@gmail.com>
2017-12-03 15:59:45 -0700

    Added in most/least ridden bike ride count methods

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -35,7 +35,9 @@ class Trip <ActiveRecord::Base
   scope :date_with_least_rides, -> {min_occurrence(:start_date)}
   scope :date_with_least_rides_trip_count, -> {group(:start_date).count.values.min}
   scope :most_ridden_bike, -> {max_occurrence(:bike_id)}
+  scope :most_ridden_bike_ride_count, -> {group(:bike_id).count.values.max}
   scope :least_ridden_bike, -> {min_occurrence(:bike_id)}
+  scope :least_ridden_bike_ride_count, -> {group(:bike_id).count.values.min}
   scope :rides_by_year, -> {group("DATE_TRUNC('year', start_date)").count}
   scope :rides_by_month, -> {group("DATE_TRUNC('month', start_date)").count}
   scope :subscription_groups, -> {group(:subscription_type)}
