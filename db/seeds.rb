3ccde38138bc778a5efe98555a2e4a9cc486a5d5
Ecksi <jimmy.dechant@gmail.com>
2017-12-03 11:40:13 -0700

    station show page link to origin / destination

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,9 @@ DatabaseCleaner.clean
 
 #seed station
 CSV.foreach('db/csv/station.csv', {headers: true, header_converters: :symbol, converters: :numeric}) do |row|
-  Station.create!(name:             row[:name],
+  Station.create!(
+                 id:                row[:id],
+                 name:              row[:name],
                  city:              row[:city],
                  dock_count:        row[:dock_count],
                  installation_date: Date.strptime(row[:installation_date], '%m/%e/%Y'),
