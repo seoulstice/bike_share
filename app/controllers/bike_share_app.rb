fc5b2ac332cf6251c819abc491dc1a575dec8b41
seoulstice <jungyoungjoon@gmail.com>
2017-12-04 19:23:51 -0700

    Feature test user_visits_conditions_index_spec written

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -109,8 +109,12 @@ class BikeShareApp < Sinatra::Base
       @paginate = params[:page]
       @pagdown = params[:page].to_i - 1
     end
-    
+
     erb :'conditions/index'
+  end
+
+  get '/conditions/new' do
+    erb :'conditions/new'
   end
 
   get '/conditions/:id' do
@@ -119,20 +123,16 @@ class BikeShareApp < Sinatra::Base
     erb :'conditions/show'
   end
 
-  get '/conditions/new' do
-    erb :'conditions/new'
+  get '/conditions/:id/edit' do
+    @condition = Condition.find(params[:id])
+
+    erb :"conditions/edit"
   end
 
   post '/conditions' do
     condition = Condition.create(params[:condition])
 
     redirect "/conditions/#{condition.id}"
-  end
-
-  get '/conditions/:id/edit' do
-    @condition = Condition.find(params[:id])
-
-    erb :"conditions/edit"
   end
 
   put '/conditions/:id' do |id|
