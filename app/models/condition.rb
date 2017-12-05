980487f0bef990cd07f6cdc167083ceeafa14323
maxscores <maxstackhouse@gmail.com>
2017-12-05 07:19:15 -0700

    Condition temp breakdown

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -5,23 +5,32 @@ class Condition < ActiveRecord::Base
     max = Condition.maximum(:max_temperature_f)
     min = Condition.minimum(:max_temperature_f)
     number_of_groups = ((max-min) / 10).ceil
-    (1..number_of_groups).map do |number|
+    (0..number_of_groups+1).map do |number|
       [(min + (number-1)*10), (min + number * 10)]
     end
   end
 
-  def self.rides_on_days_in_temp_range(lower, upper)
-    days_in_range = Condition.where("conditions.max_temperature_f >= #{lower} AND conditions.max_temperature_f <= #{upper}").select("conditions.*, avg(date) AS avg_rides")
-
-
-
-    # count_trips_in_range = days_in_range.map do |day|
-    #   day.trips.count
-    # end
-    # count_trips_in_range.sum / count_trips_in_range.count
+  def self.rides_on_days_in_temp_range_order_desc(lower, upper)
+    Condition.where("conditions.max_temperature_f > #{lower} AND conditions.max_temperature_f <= #{upper}")
+             .joins(:trips)
+             .group(:date)
+             .order("count_all DESC")
+             .count
+             .values
   end
 
-  def self.trip
+  def self.average(trips_in_range)
+    return 0 if trips_in_range.empty?
+    trips_in_range.sum / trips_in_range.count
+  end
 
+  def self.max(trips_in_range)
+    return 0 if trips_in_range.empty?
+    trips_in_range.first
+  end
+
+  def self.min(trips_in_range)
+    return 0 if trips_in_range.empty?
+    trips_in_range.last
   end
 end
