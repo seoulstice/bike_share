b5aa521f7b6c502ad5939ca2f72c6edc727007ae
Ecksi <jimmy.dechant@gmail.com>
2017-12-06 17:12:58 -0700

    5 tests fixed

--- spec/features/conditions/user_visits_conditions_index_spec.rb
+++ spec/features/conditions/user_visits_conditions_index_spec.rb
@@ -1,14 +1,4 @@
 describe "When a visitor visits conditions index" do
-  it "sees banner" do
-    visit '/conditions'
-
-    expect(page).to have_current_path('/conditions')
-
-    expect(page).to have_content("SF Bike Share")
-    expect(page).to have_link("Conditions Index")
-    expect(page).to have_link("Weather Dashboard")
-  end
-
   it "sees all stations" do
     visit '/conditions'
 
@@ -29,7 +19,7 @@ describe "When a visitor visits conditions index" do
   it "clicks on a specific condition link" do
     visit '/conditions'
 
-    click_on("Conditions on 2013-09-05")
+    click_link("Conditions on 2013-09-05")
 
     expect(page).to have_current_path('/conditions/8')
   end
@@ -37,16 +27,8 @@ describe "When a visitor visits conditions index" do
   it "click on a new condition link" do
     visit '/conditions'
 
-    click_link("New Weather Condition")
+    click_link("New Condition")
 
     expect(page).to have_current_path('/conditions/new')
-  end
-
-  it "clicks on the Home breadcrumb" do
-    visit '/conditions'
-
-    click_on("Home")
-
-    expect(page).to have_current_path('/')
   end
 end
