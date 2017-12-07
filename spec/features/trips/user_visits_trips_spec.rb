bd4d3c40e48dee12dcacbe90af25d67ed1ecb783
Ecksi <jimmy.dechant@gmail.com>
2017-12-06 23:21:51 -0700

    finish pagination tests

--- spec/features/trips/user_visits_trips_spec.rb
+++ spec/features/trips/user_visits_trips_spec.rb
@@ -35,4 +35,34 @@ describe "When a user visits trips" do
 
     expect(page).to have_current_path('/trips/new')
   end
+
+  it "only the next pagination link shows up" do
+    visit '/trips'
+
+    expect(page).to have_link("»")
+  end
+
+  it "has working pagination" do
+    visit '/trips'
+
+    click_link("»")
+
+    expect(page).to have_current_path('/trips?page=2')
+  end
+
+  it "has full pagination outside of index" do
+    visit '/trips?page=2'
+
+    expect(page).to have_link("«")
+    expect(page).to have_content("2")
+    expect(page).to have_link("»")
+  end
+
+  it "has a working previous pagination" do
+    visit '/trips?page=3'
+
+    click_link("«")
+
+    expect(page).to have_current_path('/trips?page=2')
+  end
 end
